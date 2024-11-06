3711421beb29f85fe1234bddf2579adbdbaef9a6
Clare72 <cp390@cam.ac.uk>
2024-11-06 18:41:47 +0000

    import alleles linked to exp curation

--- src/scripts/make_template.py
+++ src/scripts/make_template.py
@@ -2,7 +2,8 @@ import pandas as pd
 from collections import OrderedDict
 
 
-FB_data = pd.read_csv('tmp/FB_data_processed.tsv', sep='\t', low_memory=False)
+FB_data = pd.read_csv('tmp/FBco_data_processed.tsv', sep='\t', low_memory=False)
+exp_FBals = pd.read_csv('tmp/FBal_data_processed.tsv', sep='\t', low_memory=False)
 
 FBco_data = FB_data[['combo_id', 'combo_name', 'combo_symbol', 'combo_synonyms', 'allele_id']]
 FBco_data = FBco_data.rename(columns={'combo_id':'ID', 'combo_name':'Name', 'combo_symbol': 'Symbol', 'combo_synonyms':'Synonyms', 'allele_id':'Hemidriver'})
@@ -12,8 +13,15 @@ FBco_data['Xref'] = 'FlyBase'
 
 FBal_data = FB_data[['allele_id', 'allele_name', 'allele_synonyms', 'tool_fbcv']]
 FBal_data = FBal_data.rename(columns={'allele_id':'ID', 'allele_name':'Name', 'allele_synonyms':'Synonyms', 'tool_fbcv':'Parent'})
+FBal_data['Parent'] = FBal_data['Parent'].apply(lambda x: str(x) + '|SO:0001023')
 FBal_data['TYPE'] = 'owl:Class'
 FBal_data['Xref'] = 'FlyBase'
+
+exp_FBal_data = exp_FBals[['allele_id', 'allele_name', 'allele_synonyms']]
+exp_FBal_data = exp_FBal_data.rename(columns={'allele_id':'ID', 'allele_name':'Name', 'allele_synonyms':'Synonyms'})
+exp_FBal_data['Parent'] = 'SO:0001023'
+exp_FBal_data['TYPE'] = 'owl:Class'
+exp_FBal_data['Xref'] = 'FlyBase'
 
 template_header = pd.DataFrame({'ID':['ID'], 'TYPE': ['TYPE'], 
                         "Name": ["A rdfs:label SPLIT=|"],
@@ -23,6 +31,6 @@ template_header = pd.DataFrame({'ID':['ID'], 'TYPE': ['TYPE'],
                         "Hemidriver": ["C VFBext:0000008 some %"],
                         "Xref": ["A oboInOwl:hasDbXref"]})
 
-template = pd.concat([template_header, FBco_data, FBal_data], ignore_index=True)
+template = pd.concat([template_header, FBco_data, FBal_data, exp_FBal_data], ignore_index=True)
 
 template.to_csv('tmp/template.tsv', sep='\t', index=None)
